da91877c8354d67c5fc358f66e6b01ec3df538d3
Michael Vanasse <michaelvanasse@gmail.com>
2013-11-08 18:31:31 -0700

    remove js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,30 +1,3 @@
 
-window.dom = require('dom');
-window.query = require('query');
+exports = module.exports = function(){};
 
-
-// module.exports = function(app) {
-//   console.log('!!!!!!!!!!!!!!!!!', app);
-//   app.directive('zzz', function() {
-//       return {
-//         template: '<h1>zoxcijzoicxj</h1>'
-//       };
-//     });
-// };
-// 
-
-exports = module.exports = Shoelace;
-
-function Shoelace(app) {
-  if (!(this instanceof Shoelace)) return new Shoelace(app);
-  return window.shoelace = this;
-}
-
-// Shoelace.prototype = {
-//   toggle: function(id) {
-//     window.el = document.getElementById(id);
-//     el.className = el.className === "on"
-//       ? "off"
-//       : "on";
-//   }
-// };
